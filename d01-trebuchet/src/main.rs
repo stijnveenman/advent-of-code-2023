4148438a695a9237a661698fdc21a680f7d88e1f
Stijn Veenman <stijn.veenman@schiphol.nl>
2023-12-01 17:51:06 +0100

    finished part 2 day 1

--- d01-trebuchet/src/main.rs
+++ d01-trebuchet/src/main.rs
@@ -1,15 +1,48 @@
+use regex::Regex;
+
 fn main() {
     let input = include_str!("./input.txt");
 
     println!("{}", process(input))
 }
 
+fn replace_line(s: &str) -> String {
+    let mut s = s.to_string();
+
+    s = s.replace("one", "1");
+    s = s.replace("two", "2");
+    s = s.replace("three", "3");
+    s = s.replace("four", "4");
+    s = s.replace("five", "5");
+    s = s.replace("six", "6");
+    s = s.replace("seven", "7");
+    s = s.replace("eight", "8");
+    s = s.replace("nine", "9");
+
+    s
+}
+
 fn process_line(s: &str) -> u32 {
-    let numbers = s.chars().filter(|c| c.is_numeric()).collect::<Vec<_>>();
-    let first = numbers.first().unwrap();
-    let last = numbers.last().unwrap();
+    print!("{} ", s);
+    let first = Regex::new(r"(\d|one|two|three|four|five|six|seven|eight|nine).*")
+        .unwrap()
+        .captures(s)
+        .unwrap()
+        .get(1)
+        .unwrap();
+    let first = replace_line(first.as_str());
+
+    let last = Regex::new(r".*(\d|one|two|three|four|five|six|seven|eight|nine)")
+        .unwrap()
+        .captures(s)
+        .unwrap()
+        .get(1)
+        .unwrap();
+    let last = replace_line(last.as_str());
 
     let number = format!("{}{}", first, last);
+
+    println!("{}", number);
     number.parse().unwrap()
 }
 
@@ -29,4 +62,19 @@ treb7uchet";
     let result = process(input);
 
     assert_eq!(dbg!(result), 142)
+}
+
+#[test]
+fn test_input2() {
+    let input = "two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen";
+
+    let result = process(input);
+
+    assert_eq!(dbg!(result), 281)
 }
