f1f6f642356bd3a0839248ae1348375fef34077f
Stijn Veenman <stijn.veenman@schiphol.nl>
2023-12-08 15:35:43 +0100

    finished day 7

--- d07-camel-cards/src/main.rs
+++ d07-camel-cards/src/main.rs
@@ -1,7 +1,9 @@
 #![allow(dead_code)]
 #![allow(unused_variables)]
 
-use std::{cmp::Ordering, collections::HashMap};
+use std::collections::HashMap;
+
+use rstest::rstest;
 
 fn main() {
     let input = include_str!("./input.txt");
@@ -13,49 +15,6 @@ fn main() {
 struct Hand<'a> {
     cards: &'a str,
     bet: u32,
-}
-
-impl<'a> Hand<'a> {
-    fn get_type(&self) -> u32 {
-        let hm = self.hashmap();
-
-        if hm.iter().any(|c| *c.1 == 5) {
-            return 7;
-        }
-
-        if hm.iter().any(|c| *c.1 == 4) {
-            return 6;
-        }
-
-        if hm.iter().any(|c| *c.1 == 2) && hm.iter().any(|c| *c.1 == 3) {
-            return 5;
-        }
-
-        if hm.iter().any(|c| *c.1 == 3) {
-            return 4;
-        }
-
-        if hm.iter().filter(|c| *c.1 == 2).count() == 2 {
-            return 3;
-        }
-
-        if hm.iter().any(|c| *c.1 == 2) {
-            return 2;
-        }
-
-        1
-    }
-
-    fn hashmap(&self) -> HashMap<char, u32> {
-        let mut h = HashMap::new();
-
-        self.cards.chars().for_each(|c| {
-            let count = h.get(&c).unwrap_or(&0);
-            h.insert(c, count + 1);
-        });
-
-        h
-    }
 }
 
 impl<'a> PartialOrd for Hand<'a> {
@@ -73,7 +32,7 @@ fn to_val(c: char) -> u32 {
         'A' => 14,
         'K' => 13,
         'Q' => 12,
-        'J' => 11,
+        'J' => 1, // 1 for part 2, 11 for part 1
         'T' => 10,
         _ => panic!("wrong card"),
     }
@@ -111,7 +70,11 @@ fn process(s: &str) -> u32 {
 
     hands.sort();
 
-    hands.iter().for_each(|h| println!("{:?}", h));
+    hands.iter().for_each(|h| {
+        if h.cards.chars().filter(|c| *c == 'J').count() > 2 {
+            println!("{} {}", h.cards, h.get_type());
+        }
+    });
 
     hands
         .iter()
@@ -120,15 +83,76 @@ fn process(s: &str) -> u32 {
         .sum()
 }
 
-fn process2(s: &str) -> u32 {
-    todo!()
-}
-
 static TEST_INPUT: &str = "32T3K 765
 T55J5 684
 KK677 28
 KTJJT 220
 QQQJA 483";
+
+impl<'a> Hand<'a> {
+    fn get_type(&self) -> u32 {
+        let mut hm = self.hashmap();
+        let jacks = *hm.get(&'J').unwrap_or(&0);
+        hm.remove(&'J');
+
+        if jacks == 5 {
+            return 7;
+        }
+
+        if hm.iter().any(|c| *c.1 + jacks >= 5) {
+            return 7;
+        }
+
+        if hm.iter().any(|c| *c.1 + jacks >= 4) {
+            return 6;
+        }
+
+        if jacks == 1 {
+            if hm.iter().any(|c| *c.1 == 3) {
+                return 5;
+            }
+            if hm.iter().filter(|c| *c.1 == 2).count() == 2 {
+                return 5;
+            }
+        }
+
+        if hm.iter().any(|c| *c.1 == 2) && hm.iter().any(|c| *c.1 == 3) {
+            return 5;
+        }
+
+        if hm.iter().any(|c| *c.1 + jacks >= 3) {
+            return 4;
+        }
+
+        if hm.iter().filter(|c| *c.1 == 2).count() == 2 {
+            return 3;
+        }
+        if hm.iter().any(|c| *c.1 == 2) && jacks == 1 {
+            return 3;
+        }
+
+        if hm.iter().any(|c| *c.1 == 2) {
+            return 2;
+        }
+
+        if jacks == 1 {
+            return 2;
+        }
+
+        1
+    }
+
+    fn hashmap(&self) -> HashMap<char, u32> {
+        let mut h = HashMap::new();
+
+        self.cards.chars().for_each(|c| {
+            let count = h.get(&c).unwrap_or(&0);
+            h.insert(c, count + 1);
+        });
+
+        h
+    }
+}
 
 #[test]
 fn test_part1() {
@@ -139,7 +163,23 @@ fn test_part1() {
 
 #[test]
 fn test_part2() {
-    let result = process2(TEST_INPUT);
+    let result = process(TEST_INPUT);
 
-    assert_eq!(dbg!(result), 0)
+    assert_eq!(dbg!(result), 5905)
+}
+
+#[rstest]
+#[case("JJQQQ", 7)]
+#[case("JJKQQ", 6)]
+#[case("JQQKK", 5)]
+#[case("JQQKK", 5)]
+fn hands(#[case] input: &str, #[case] expected: u32) {
+    assert_eq!(
+        Hand {
+            cards: input,
+            bet: 0
+        }
+        .get_type(),
+        expected
+    );
 }
