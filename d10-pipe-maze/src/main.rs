7e9dc4385f37d7d8f9e302c80c7d3de7778bba4c
Stijn Veenman <stijn.veenman@schiphol.nl>
2023-12-10 10:40:55 +0100

    setup next tiles

--- d10-pipe-maze/src/main.rs
+++ d10-pipe-maze/src/main.rs
@@ -11,7 +11,7 @@ fn main() {
     println!("{}", process(input))
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Clone, Copy)]
 enum PipeTile {
     Vertical,
     Horizontal,
@@ -39,18 +39,60 @@ impl From<char> for PipeTile {
     }
 }
 
+impl PipeTile {
+    fn next(&self, pos: (u32, u32)) -> Option<((u32, u32), (u32, u32))> {
+        match self {
+            PipeTile::Vertical => Some(((pos.0, pos.1 + 1), (pos.0, pos.1 - 1))),
+            PipeTile::Horizontal => Some(((pos.0 + 1, pos.1), (pos.0 - 1, pos.1))),
+            PipeTile::UpRight => Some(((pos.0, pos.1 - 1), (pos.0 + 1, pos.1))),
+            PipeTile::UpLeft => Some(((pos.0, pos.1 - 1), (pos.0 - 1, pos.1))),
+            PipeTile::DownLeft => Some(((pos.0, pos.1 + 1), (pos.0 - 1, pos.1))),
+            PipeTile::DownRight => Some(((pos.0, pos.1 + 1), (pos.0 + 1, pos.1))),
+            PipeTile::Start => None,
+            PipeTile::Ground => None,
+        }
+    }
+}
+
+#[allow(clippy::ptr_arg)]
+fn find_start(i: &Vec<Vec<PipeTile>>) -> (u32, u32) {
+    i.iter()
+        .enumerate()
+        .find(|l| l.1.iter().any(|t| *t == PipeTile::Start))
+        .map(|i| {
+            (
+                i.0 as u32,
+                i.1.iter()
+                    .enumerate()
+                    .find(|t| *t.1 == PipeTile::Start)
+                    .unwrap()
+                    .0 as u32,
+            )
+        })
+        .unwrap()
+}
+
+#[allow(clippy::ptr_arg)]
+fn get(i: &Vec<Vec<PipeTile>>, pos: (u32, u32)) -> PipeTile {
+    *i.get(pos.1 as usize).unwrap().get(pos.0 as usize).unwrap()
+}
+
+fn find_loop(i: Vec<Vec<PipeTile>>) {
+    let mut start = find_start(&i);
+    println!("{:?}", (start, get(&i, start).next(start)));
+}
+
 fn parse(s: &str) -> Vec<Vec<PipeTile>> {
     s.lines()
         .map(|l| l.chars().map(|c| c.into()).collect())
-        .dbg()
         .collect()
 }
 
 fn process(s: &str) -> u32 {
     let input = parse(s);
-    println!("{:?}", input);
+    find_loop(input);
 
-    todo!()
+    420
 }
 
 fn process2(s: &str) -> u32 {
@@ -61,7 +103,7 @@ fn process2(s: &str) -> u32 {
 }
 
 static TEST_INPUT: &str = ".....
-.F-7.
+.S-7.
 .|.|.
 .L-J.
 .....";
