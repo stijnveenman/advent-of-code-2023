06b691d4114351ddf1c3a10e917034d3e9ae4806
Stijn Veenman <stijn.veenman@schiphol.nl>
2023-12-11 21:47:04 +0100

    parsed pionts

--- d11-cosmic-expansion/src/main.rs
+++ d11-cosmic-expansion/src/main.rs
@@ -1,13 +1,14 @@
 #![allow(dead_code)]
 #![allow(unused_variables)]
 use nom::{
-    bytes::complete::is_a,
+    bytes::complete::{is_a, tag},
     character::complete::{self},
     multi::many0,
     sequence::preceded,
-    IResult,
+    IResult, Parser,
 };
 mod util;
+use nom_locate::LocatedSpan;
 #[allow(unused_imports)]
 use util::*;
 
@@ -17,19 +18,34 @@ fn main() {
     println!("{}", process(input))
 }
 
-fn parse(s: &str) -> IResult<&str, Vec<char>> {
-    many0(preceded(is_a(".\n"), complete::char('#')))(s)
+type Span<'a> = LocatedSpan<&'a str>;
+
+#[derive(Debug)]
+struct Point {
+    x: isize,
+    y: isize,
+}
+
+fn as_point(span: Span) -> Point {
+    Point {
+        x: span.get_column() as isize - 1,
+        y: span.location_line() as isize - 1,
+    }
+}
+
+fn parse(s: Span) -> IResult<Span, Vec<Point>> {
+    many0(preceded(is_a(".\n"), tag("#").map(as_point)))(s)
 }
 
 fn process(s: &str) -> u32 {
-    let (_, input) = parse(s).unwrap();
+    let (_, input) = parse(LocatedSpan::new(s)).unwrap();
     println!("{:?}", input);
 
     todo!()
 }
 
 fn process2(s: &str) -> u32 {
-    let (_, input) = parse(s).unwrap();
+    let (_, input) = parse(LocatedSpan::new(s)).unwrap();
     println!("{:?}", input);
 
     todo!()
