7c0c8dbda947e0ef2a2a706c33601152c5c9d64c
Stijn Veenman <stijn.veenman@schiphol.nl>
2023-12-11 21:59:53 +0100

    added expand

--- d11-cosmic-expansion/src/main.rs
+++ d11-cosmic-expansion/src/main.rs
@@ -2,7 +2,6 @@
 #![allow(unused_variables)]
 use nom::{
     bytes::complete::{is_a, tag},
-    character::complete::{self},
     multi::many0,
     sequence::preceded,
     IResult, Parser,
@@ -20,7 +19,7 @@ fn main() {
 
 type Span<'a> = LocatedSpan<&'a str>;
 
-#[derive(Debug)]
+#[derive(Debug, Clone, Copy)]
 struct Point {
     x: isize,
     y: isize,
@@ -37,9 +36,44 @@ fn parse(s: Span) -> IResult<Span, Vec<Point>> {
     many0(preceded(is_a(".\n"), tag("#").map(as_point)))(s)
 }
 
+fn get_size(points: &[Point]) -> Point {
+    Point {
+        x: points.iter().map(|p| p.x).max().unwrap(),
+        y: points.iter().map(|p| p.y).max().unwrap(),
+    }
+}
+
+fn expand(points: Vec<Point>) -> Vec<Point> {
+    let size = get_size(points.as_slice());
+
+    let empty_cols = (0..size.x)
+        .filter(|x| points.iter().all(|p| p.x != *x))
+        .collect::<Vec<_>>();
+    let empty_rows = (0..size.y)
+        .filter(|y| points.iter().all(|p| p.y != *y))
+        .collect::<Vec<_>>();
+
+    points
+        .into_iter()
+        .map(|p| {
+            let dx = empty_cols.iter().filter(|c| p.x > **c).count() as isize;
+            let dy = empty_rows.iter().filter(|c| p.y > **c).count() as isize;
+
+            Point {
+                x: p.x + dx,
+                y: p.y + dy,
+            }
+        })
+        .dbg()
+        .collect()
+}
+
 fn process(s: &str) -> u32 {
-    let (_, input) = parse(LocatedSpan::new(s)).unwrap();
-    println!("{:?}", input);
+    let (_, points) = parse(LocatedSpan::new(s)).unwrap();
+
+    let points = expand(points);
+
+    println!("{:?}", points);
 
     todo!()
 }
