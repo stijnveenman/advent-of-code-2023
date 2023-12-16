351819f592c904900c9a0f91ae15ec13271806b7
Stijn Veenman <stijn.veenman@schiphol.nl>
2023-12-16 14:30:29 +0100

    finished d13 p1

--- d13-point-of-incidence/src/main.rs
+++ d13-point-of-incidence/src/main.rs
@@ -14,7 +14,10 @@ fn check_horizontal(lines: &[&str], i: usize) -> bool {
     let mut j = 0;
 
     loop {
-        let Some(bottom) = lines.get(i - 1 - j) else {
+        let Some(idx) = i.checked_sub(1).and_then(|i| i.checked_sub(j)) else {
+            return true;
+        };
+        let Some(bottom) = lines.get(idx) else {
             return true;
         };
         let Some(top) = lines.get(i + j) else {
@@ -29,25 +32,51 @@ fn check_horizontal(lines: &[&str], i: usize) -> bool {
     }
 }
 
-fn reflect_horizontal(s: &str) -> Option<usize> {
-    let lines = s.lines().collect::<Vec<_>>();
+fn reflect(lines: &[&str]) -> Option<usize> {
     for i in 1..lines.len() {
         let prev = lines[i - 1];
         let cur = lines[i];
 
-        if cur == prev && check_horizontal(&lines, i) {
+        if cur == prev && check_horizontal(lines, i) {
             return Some(i);
         }
     }
     None
 }
 
+fn reflect_horizontal(s: &str) -> Option<usize> {
+    let lines = s.lines().collect::<Vec<_>>();
+    reflect(&lines)
+}
+
+fn flip(s: &str) -> Vec<String> {
+    let mut lines = s.lines().map(|c| c.chars()).collect::<Vec<_>>();
+    let mut r = vec![];
+
+    while let Some(s) = lines
+        .iter_mut()
+        .map(|c| c.next())
+        .collect::<Option<String>>()
+    {
+        r.push(s);
+    }
+
+    r
+}
+
+fn reflect_vertical(s: &str) -> Option<usize> {
+    let lines = flip(s);
+    reflect(&lines.iter().map(|s| s.as_str()).collect::<Vec<_>>())
+}
+
 fn reflection(s: &str) -> usize {
-    reflect_horizontal(s).unwrap_or(0)
+    reflect_horizontal(s)
+        .map(|r| r * 100)
+        .unwrap_or_else(|| reflect_vertical(s).unwrap())
 }
 
 fn process(s: &str) -> usize {
-    s.split("\n\n").map(reflection).dbg().sum()
+    s.split("\n\n").map(reflection).sum()
 }
 
 fn process2(s: &str) -> usize {
