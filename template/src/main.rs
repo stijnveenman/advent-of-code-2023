9e687eb167acb8b5dbbfc20cbc807837f293106a
Stijn Veenman <stijn.veenman@schiphol.nl>
2023-12-19 16:12:00 +0100

    update template

--- template/src/main.rs
+++ template/src/main.rs
@@ -1,13 +1,12 @@
 #![allow(dead_code)]
 #![allow(unused_variables)]
-use nom::{
-    character::complete::{newline, space1},
-    multi::separated_list1,
-    IResult,
-};
 mod util;
 #[allow(unused_imports)]
 use util::*;
+
+static TEST_INPUT: &str = "";
+static TEST_PART1_RESULT: usize = 420;
+static TEST_PART2_RESULT: usize = 420;
 
 fn main() {
     let input = include_str!("./input.txt");
@@ -15,36 +14,34 @@ fn main() {
     println!("{}", process(input))
 }
 
-fn parse(s: &str) -> IResult<&str, Vec<Vec<u32>>> {
-    separated_list1(newline, separated_list1(space1, u32))(s)
+fn parse(s: &str) -> &str {
+    s
 }
 
 fn process(s: &str) -> usize {
-    let (_, input) = parse(s).unwrap();
+    let input = parse(s);
     println!("{:?}", input);
 
     todo!()
 }
 
 fn process2(s: &str) -> usize {
-    let (_, input) = parse(s).unwrap();
+    let input = parse(s);
     println!("{:?}", input);
 
     todo!()
 }
 
-static TEST_INPUT: &str = "";
-
 #[test]
 fn test_part1() {
     let result = process(TEST_INPUT);
 
-    assert_eq!(dbg!(result), 0)
+    assert_eq!(dbg!(result), TEST_PART1_RESULT)
 }
 
 #[test]
 fn test_part2() {
     let result = process2(TEST_INPUT);
 
-    assert_eq!(dbg!(result), 0)
+    assert_eq!(dbg!(result), TEST_PART2_RESULT)
 }
